4e4c9dfc2e6117675513d0dc8fb4a4d363d7c5c3
Susan J. Miller <sjmiller@email.arizona.edu>
2019-11-20 10:16:52 -0700

    new code for stemming

--- paper-paper-network/coursecat_processor.py
+++ paper-paper-network/coursecat_processor.py
@@ -5,43 +5,26 @@ from nltk import Tree
 import pandas as pd
 import spacy
 from nltk.stem import PorterStemmer
+import numpy
 
 nlp = spacy.load('en_core_web_sm')
-
 
 from nltk.stem import PorterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
 ps=PorterStemmer()
 
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
 
 filename="/Users/sjmiller/CourseCat/DataSciCourses_IDTitleFull.csv"
-numberofrec=100000
+#maxrec=100000
+maxrec=3
 thresold=3
 
-
-df=pd.read_csv(filename)
-df=df[0:numberofrec]
-# Defining a grammar & Parser
-NP = "NP: {(<V\w+>|<NN\w?>)+.*<NN\w?>}"
-chunker = RegexpParser(NP)
-
-def get_continuous_chunks(text, chunk_func=ne_chunk):
-    chunked = chunk_func(pos_tag(word_tokenize(text)))
-    continuous_chunk = []
-    current_chunk = []
-    for subtree in chunked:
-        if type(subtree) == Tree:
-            current_chunk.append(" ".join([token for token, pos in subtree.leaves()]))
-        elif current_chunk:
-            named_entity = " ".join(current_chunk)
-            if named_entity not in continuous_chunk:
-                continuous_chunk.append(named_entity)
-                current_chunk = []
-        else:
-            continue
-    return continuous_chunk
+df=pd.read_csv(filename, nrows = maxrec)
+print('Read %d lines' % df.shape[0])
+maxrec = df.shape[0]
+df=df[0:maxrec]
 
 def processdata(data):
 
@@ -50,7 +33,9 @@ def processdata(data):
     tokens = []
     for token in doc:
         if (token.is_alpha and not token.is_stop):
-            tokens.append(ps.stem(token.lemma_))
+            stem = ps.stem(token.lemma_)
+            if (len(stem) > 3):
+                tokens.append(stem)
 
     return ' '.join(tokens)
 
@@ -60,26 +45,24 @@ def processdata(data):
 #exit()
 
 df['text']=df['Course_Title'].astype(str) + " " + df["Full_Course_Description"].astype(str) 
+df['stem']=df['text'].apply(processdata)
 
-df['np']=df['text'].apply(lambda sent: get_continuous_chunks(sent, chunker.parse))
-
-stemtxt=[]
-
-for i in range(0, len(df)):
-	txt=' '.join(df['np'][i]).split(" ")
-	stext= [ps.stem(t) for t in txt ]
-	stext=[ k for k in stext if len(k)>thresold ]
-	a=' '.join(stext)
-	stemtxt.append(a)
-	if i % 500:
-		print("progress " , i , "out of ", numberofrec , " \r", end='')
-
-df['stem']=stemtxt	
 savestemdf=df[['CRSE_ID','stem']]
-outfilename="coursecat_stem_"+str(numberofrec)+".csv"
+outfilename="coursecat_stem_"+str(maxrec)+".csv"
 savestemdf.to_csv(outfilename,index=False)
 print("Done processing. File saved:", outfilename )	 
 #import pdb; pdb.set_trace()
 
 
+dist = numpy.zeros((maxrec, maxrec))
+
+def intersection(lst1, lst2): 
+  
+    # Use of hybrid method 
+    temp = set(lst2) 
+    lst3 = [value for value in lst1 if value in temp] 
+    return lst3 
+
+print(len(intersection(i,j) for i in df['stem'] for j in df['stem']) )
+   
 
